e17095dde5aa379d3a028de8c11900446255f525
Robin Heggelund Hansen <git@heghan.org>
2024-11-24 22:13:40 +0100

    Add a test for reading stdin and outputting to stdout.

--- streams/test/commands.mjs
+++ streams/test/commands.mjs
@@ -3,11 +3,19 @@ import { runner } from "clet";
 
 const baseDir = path.resolve("bin");
 
-describe("Requests", () => {
+describe("Streams", () => {
   it("Hello, world", async () => {
     await runner()
       .cwd(baseDir)
       .fork("app", ["hello world"], {})
       .stdout("Hello, world!");
+  });
+  
+  it("input and output", async () => {
+    await runner()
+      .cwd(baseDir)
+      .fork("app", ["greet"], {})
+      .stdin(/name:/, "gren fanboy")
+      .stdout("Hello gren fanboy\n")
   });
 });
