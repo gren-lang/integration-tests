a9191af0984a025a1d87493c2d7b6fbce26dd03b
Robin Heggelund Hansen <git@heghan.org>
2024-11-26 23:32:47 +0100

    More Stream tests.

--- streams/test/commands.mjs
+++ streams/test/commands.mjs
@@ -1,9 +1,14 @@
 import * as path from "node:path";
+import * as fs from "node:fs";
 import { runner } from "clet";
 
 const baseDir = path.resolve("bin");
 
 describe("Streams", () => {
+  after(() => {
+    fs.rmSync(path.resolve("bin/compressed.txt"))
+  })
+  
   it("Hello, world", async () => {
     await runner()
       .cwd(baseDir)
@@ -17,5 +22,28 @@ describe("Streams", () => {
       .fork("app", ["greet"], {})
       .stdin(/name:/, "gren fanboy")
       .stdout("Hello gren fanboy\n")
+  });
+  
+  it("fromArray", async () => {
+    await runner()
+      .cwd(baseDir)
+      .fork("app", ["fromArray"], {})
+      .stdout("onetwofour")
+      .stdout("CLOSED");
+  });
+  
+  it("compression", async () => {
+    await runner()
+      .cwd(baseDir)
+      .fork("app", ["compression"], {})
+      .stdin(/input:/, "this is a very\n long compression test\n")
+      .stdout("Done")
+  });
+  
+  it("decompression", async () => {
+    await runner()
+      .cwd(baseDir)
+      .fork("app", ["decompression"], {})
+      .stdout("this is a very\n long compression test")
   });
 });
